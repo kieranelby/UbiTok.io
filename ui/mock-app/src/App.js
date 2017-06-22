8f7f32c67d8c87e59e72a290086645cbcff944e1
Kieran Elby <kieran@dunelm.org.uk>
2017-06-22 23:30:34 +0100

    use react-bootstrap, start basic rendering

--- ui/mock-app/src/App.js
+++ ui/mock-app/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col, Table, Button, FormGroup, FormControl, ControlLabel, HelpBlock, Tab, Tabs } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      // what is this place?
+      // where are we?
       "app": {
         "siteName": "UbiTok.io"
       },
@@ -40,6 +41,8 @@ class App extends Component {
         "approvedButNotTransferred": ["", ""],
         "exchange": ["", ""]
       },
+      // TODO - some sort of deposit/withdraw/approve form
+      // payments we've made
       "myPayments": {
         // is this a complete record, or should we offer a 'Loading ...' indicator or
         // a 'Show More ...' button? (TODO - how to know which - two different things?)
@@ -67,6 +70,22 @@ class App extends Component {
             ["Buy@1.23", "5000000"]
         ]
       },
+      // an order the user is preparing
+      "createOrder": {
+        // have they selected buy or sell base?
+        "side": "buy",
+        "buy": {
+          "amountBase": "",
+          "price": "",
+          "costQuoted": ""
+        },
+        "sell": {
+          "amountBase": "",
+          "price": "",
+          "costQuoted": ""
+        }
+      },
+      // orders we've created
       "myOrders": {
         // is this a complete record, or should we offer a 'Loading ...' indicator or
         // a 'Show More ...' button? (TODO - how to know which - two different things?)
@@ -76,10 +95,10 @@ class App extends Component {
         // perhaps contract can maintain lastOrderIdForClient + clientPrevOrderId? adds gas costs tho.
         // used when finding history with EVM events
         "startBlock": null,
-        "orderIds": ["101"],
-        "byId": {
+        "order": [
           // TODO - time? blockId?
-          "101": {
+          {
+            "orderId": "101",
             "price": "Buy@0.123",
             "sizeBase": "2000000",
             "terms": "GoodTillCancel",
@@ -88,8 +107,9 @@ class App extends Component {
             "executedBase": "500000",
             "executedQuoted": "50000"
           }
-        }
+        ]
       },
+      // trades that have happened in the market
       "marketTrades": {
         // is this a complete record, or should we offer a 'Loading ...' indicator or
         // a 'Show More ...' button? (TODO - how to know which - two different things?)
@@ -108,6 +128,36 @@ class App extends Component {
       }
     };
   }
+  getValidationState() {
+    const length = this.state.createOrder.buy.amountBase.length;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
+  }
+  handleCreateOrderSideSelect = (e) => {
+    var v = e; // no event object for this one?
+    this.setState((prevState, props) => {
+      prevState.createOrder.side = v;
+    });
+  }
+  handleCreateOrderBuyAmountBaseChange = (e) => {
+    var v = e.target.value;
+    this.setState((prevState, props) => {
+      prevState.createOrder.buy.amountBase = v;
+    });
+  }
+  handleCreateOrderBuyPriceChange = (e) => {
+    var v = e.target.value;
+    this.setState((prevState, props) => {
+      prevState.createOrder.buy.price = v;
+    });
+  }
+  handleCreateOrderBuyCostCounterChange = (e) => {
+    var v = e.target.value;
+    this.setState((prevState, props) => {
+      prevState.createOrder.buy.costCounter = v;
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -118,36 +168,151 @@ class App extends Component {
         <p className="App-intro">
           This is a mock-up using hard-coded data in <code>src/App.js</code>.
         </p>
-        <h3>Book Info (what you are trading)</h3>
-        <p>
-          <dl>
-            <dt>Base Symbol</dt>
-            <dd>{this.state.pairInfo.base.symbol}</dd>
-            <dt>Base Tradable Type</dt>
-            <dd>{this.state.pairInfo.base.tradableType}</dd>
-            <dt>Base Minimum Size</dt>
-            <dd>{this.state.pairInfo.base.minInitialSize}</dd>
-            <dt>Counter Symbol</dt>
-            <dd>{this.state.pairInfo.counter.symbol}</dd>
-            <dt>Counter Tradable Type</dt>
-            <dd>{this.state.pairInfo.counter.tradableType}</dd>
-            <dt>Counter Minimum Size</dt>
-            <dd>{this.state.pairInfo.counter.minInitialSize}</dd>
-            <dt>Minimum Price</dt>
-            <dd>{this.state.pairInfo.minPrice}</dd>
-            <dt>Maximum Price</dt>
-            <dd>{this.state.pairInfo.maxPrice}</dd>
-          </dl>
-        </p>
-        <h3>My Balances</h3>
-        <h3>My Payments</h3>
-        <h3>Order Book</h3>
-        <h4>Asks (offers to sell UBI)</h4>
-        <h4>Bids (offers to buy UBI)</h4>
-        <h3>Create Order</h3>
-        <h3>My Orders</h3>
-        <h3>Market Trade History</h3>
-        <h3>Market Price History</h3>
+        <Grid>
+          <Row>
+            <Col md={4}>
+                  <h3>Products</h3>
+                  <Table striped bordered condensed hover>
+                    <thead>
+                      <tr>
+                        <th></th>
+                        <th>Base</th>
+                        <th>Counter</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Symbol</td>
+                        <td>{this.state.pairInfo.base.symbol}</td>
+                        <td>{this.state.pairInfo.counter.symbol}</td>
+                      </tr>
+                      <tr>
+                        <td>Type</td>
+                        <td>{this.state.pairInfo.base.tradableType}</td>
+                        <td>{this.state.pairInfo.counter.tradableType}</td>
+                      </tr>
+                      <tr>
+                        <td>Minimum Size</td>
+                        <td>{this.state.pairInfo.base.minInitialSize}</td>
+                        <td>{this.state.pairInfo.counter.minInitialSize}</td>
+                      </tr>
+                      <tr>
+                        <td>Minimum Price</td>
+                        <td colSpan="2">{this.state.pairInfo.minPrice}</td>
+                      </tr>
+                      <tr>
+                        <td>Maximum Price</td>
+                        <td colSpan="2">{this.state.pairInfo.maxPrice}</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                  <h3>My Balances</h3>
+                  <Table striped bordered condensed hover>
+                    <thead>
+                      <tr>
+                        <th>Symbol</th>
+                        <th>Exchange Balance</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{this.state.pairInfo.base.symbol}</td>
+                        <td>0.0</td>
+                      </tr>
+                      <tr>
+                        <td>{this.state.pairInfo.counter.symbol}</td>
+                        <td>0.0</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+            </Col>
+            <Col md={4}>
+              <h3>Order Book</h3>
+              <h4>Asks (offers to sell UBI)</h4>
+              <table>
+                {this.state.book.asks.map((entry) =>
+                <tr key={entry[0]}>
+                  <td>{entry[0]}</td><td>{entry[1]}</td>
+                </tr>
+                )}
+              </table>
+              <h4>Bids (offers to buy UBI)</h4>
+                <Table striped bordered condensed hover>
+                  <thead>
+                    <tr>
+                      <th>Price</th>
+                      <th>Depth ({this.state.pairInfo.base.symbol})</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.book.bids.map((entry) =>
+                    <tr key={entry[0]}>
+                      <td>{entry[0]}</td>
+                      <td>{entry[1]}</td>
+                    </tr>
+                    )}
+                  </tbody>
+                </Table>
+            </Col>
+            <Col md={4}>
+              <h3>Price History</h3>
+              <h3>Market Trades</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={4}>
+              <h3>Create Order</h3>
+              <Tabs activeKey={this.state.createOrder.side} onSelect={this.handleCreateOrderSideSelect} id="create-order-side">
+                <Tab eventKey={"buy"} title={"BUY " + this.state.pairInfo.base.symbol}>
+                  <form>
+                    <FormGroup controlId="createOrderBuy" validationState={this.getValidationState()}>
+                      <ControlLabel>Amount ({this.state.pairInfo.base.symbol})</ControlLabel>
+                      <FormControl
+                        type="text"
+                        value={this.state.createOrder.buy.amountBase}
+                        placeholder={"How many " + this.state.pairInfo.base.symbol + " to buy"}
+                        onChange={this.handleCreateOrderBuyAmountBaseChange}
+                      />
+                      <FormControl.Feedback />
+                      <ControlLabel>Price</ControlLabel>
+                      <FormControl
+                        type="text"
+                        value={this.state.createOrder.buy.price}
+                        placeholder="Enter text"
+                        onChange={this.handleCreateOrderBuyPriceChange}
+                      />
+                      <FormControl.Feedback />
+                      <ControlLabel>Cost ({this.state.pairInfo.counter.symbol})</ControlLabel>
+                      <FormControl
+                        type="text"
+                        value={this.state.createOrder.buy.costCounter}
+                        placeholder={"How much " + this.state.pairInfo.counter.symbol + " this order will cost"}
+                        onChange={this.handleCreateOrderBuyCostCounterChange}
+                      />
+                      <FormControl.Feedback />
+                      <ControlLabel>Terms</ControlLabel>
+                      <FormControl componentClass="select" placeholder="select">
+                        <option value="GoodTillCancel">Good Till Cancel</option>
+                        <option value="Immediate Or Cancel">Immediate Or Cancel</option>
+                        <option value="MakerOnly">Maker Only</option>
+                      </FormControl>
+                      <Button type="submit">
+                        PLACE BUY ORDER
+                      </Button>
+                      <HelpBlock>Validation is based on string length.</HelpBlock>
+                    </FormGroup>
+                  </form>
+                </Tab>
+                <Tab eventKey={"sell"} title={"SELL " + this.state.pairInfo.base.symbol}>
+                  Tab 2 content
+                </Tab>
+              </Tabs>
+            </Col>
+            <Col md={8}>
+              <h3>My Orders</h3>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
